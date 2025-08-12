51af061faf6b9e23db1e8ce2e3888c15ed01e49e
Daniella Sayson <88775636+Neverbeast24@users.noreply.github.com>
2025-08-12 14:28:01 +0800

    updated error login validation

--- lib/pages_admin/api_service.dart
+++ lib/pages_admin/api_service.dart
@@ -24,7 +24,7 @@ class ApiService {
 
       if (response.statusCode == 200) {
         final responseData = jsonDecode(response.body);
-        print("Login success: $responseData");
+        print("Login: $responseData");
         
         // Check if the response indicates success
         if (responseData != null && responseData.isNotEmpty) {
